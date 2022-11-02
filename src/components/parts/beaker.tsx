0922f88e5645beea8df96674d538dc4511218bd1
nakamura yusaku <nakamurayusaku@mba.local>
2022-11-02 19:14:58 +0900

    beakerのコンポーネント作成&表示

---
+++ src/components/parts/beaker.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import beakerImg from '../../beaker.png'
+
+export default function BeakerParts() {
+    return (
+        <img src={beakerImg}></img>
+    );
+}
---
+++ src/components/templates/instrument.tsx
@@ -0,0 +1,7 @@
+import BeakerParts from '../parts/beaker';
+
+export default function LabInstruments() {
+    return (
+        <BeakerParts></BeakerParts>
+    );
+}
