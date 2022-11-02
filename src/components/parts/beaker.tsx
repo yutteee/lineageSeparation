bc2bea203044b01d92ac3d104d946bf25346a11c
nakamura yusaku <nakamurayusaku@mba.local>
2022-11-02 23:53:29 +0900

    ビーカーをクリックでデータを取得できるようにした

--- src/components/parts/beaker.tsx
+++ src/components/parts/beaker.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 import beakerImg from '../../beaker.png'
 
 export default function BeakerParts() {
+    const chemicalSubstances = {
+        name: "HCl",
+        molecular: 36,
+    };
+
+    const [beakerContent, setBeakerContent] = useState({
+        name : "a",
+        molecular: 0,
+    })
+
+    const getBeakerContents = () => {
+        setBeakerContent({ ...beakerContent, name: chemicalSubstances.name, molecular: chemicalSubstances.molecular});
+    };
+
     return (
-        <img src={beakerImg}></img>
+        <>
+            <button onClick={getBeakerContents}>
+                <img src={beakerImg}></img>
+            </button>
+            <ul>
+                <li>物質: {beakerContent.name}</li>
+                <li>分子量: {beakerContent.molecular}</li>
+            </ul>
+        </>
     );
 }
