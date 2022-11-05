59d9dadb9b791c3f0f935ffa26f54f3fcc57aaca
nakamura yusaku <nakamurayusaku@mba.local>
2022-11-06 01:23:00 +0900

    reouter追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import LabInstruments from './components/templates/instrument';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LabInstruments></LabInstruments>
+      <BrowserRouter>
+        <Routes>
+          <Route path={`/`} element={<LabInstruments></LabInstruments>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
